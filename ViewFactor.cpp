4d07df75f8b3ef2bf6d5876c7255c12b38a28ace
Cormac McKinstry <66676118+C-McKinstry@users.noreply.github.com>
2021-08-26 10:51:55 +0100

    Add files via upload

--- ViewFactor.cpp
+++ ViewFactor.cpp
@@ -5,101 +5,111 @@
 #include <limits>
 #include <iostream>
 #include <MshIO/include/mshio/mshio.h>
-#include <typeinfo>
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <functions.h>
-#include <string>
 using namespace std;
-
-double sigma(double* r, double* normal_a, double* normal_b){
-  // Equal to Cos(g1)*Cos(g2) / r*r
-  double f =  dot_prod(r, normal_a) * dot_prod(r, normal_b);
-  f /= pow( dot_prod(r, r), 2 );
-  return f;
-}
 
 int main(int argc, char *argv[])
 {
   if (argc < 2){std::cout <<"You haven't entered a file\n";return 0;}
-  if (argc > 2){std::cout << "Too many arguments \n"; return 0;}
+  string pointnum = "16";
+  if(argc == 3){pointnum = argv[2];}
+  if(argc > 3){std::cout<< "Too many arguments \n";return 0;}
 
   //Import .msh file to be read
   mshio::MshSpec spec = mshio::load_msh(argv[1]);
 
-  if (spec.nodes.num_entity_blocks != 1){std::cout<< "Expected only one node block\n";return 0;}
+  vector<double> nodedata = getnodedata(spec);
+
   
-  //Find the entity block corresponding to triangles
-  int i = -1; int type;
-  while(type != 2){ 
-    i++;
-    type = spec.elements.entity_blocks[i].element_type; 
+  for(int q = 0;q<spec.elements.num_entity_blocks;q++){
+    printf("Element block %d is of type %d \n", q, spec.elements.entity_blocks[q].element_type);
+    if(spec.elements.entity_blocks[q].element_type == 2){
+      printf("Block %d has %d triangles \n",q,spec.elements.entity_blocks[q].num_elements_in_block);
     }
-
-
-  auto& triangles = spec.elements.entity_blocks[i];
-  auto& nodes = spec.nodes.entity_blocks[0];
-  int num_tri = triangles.num_elements_in_block;
-  int num_nodes = spec.nodes.num_nodes;
-
+  }
+  for(int p = 0;p<spec.nodes.num_entity_blocks;p++){
+    printf("Node block %d has %d nodes, belong to %dd entity %d \n", p,
+    spec.nodes.entity_blocks[p].num_nodes_in_block, spec.nodes.entity_blocks[p].entity_dim, spec.nodes.entity_blocks[p].entity_tag);
+  }
+  
   RTCDevice device = rtcNewDevice(NULL);
   RTCScene scene   = rtcNewScene(device);
   RTCGeometry geom;
-
+  
   float* vb;
   unsigned* ib;
-
-  double centroid_array[num_tri][3];
-  double normal_array[num_tri][3];
-  double area_array[num_tri];
-  double v[num_tri][9];
+  int num_tri = 0;
+  vector<vector<double>> centroid_array;
+  vector<vector<double>> normal_array;
+  vector<double> area_array;
+  vector<vector<double>> v;
   int n1,n2,n3;
-  for(int i = 0;i<num_tri;i++){
+  vector<double> emptyrowd;
+  
+  for(int eblock_num = 0;eblock_num<spec.elements.num_entity_blocks;eblock_num++){
+    if(spec.elements.entity_blocks[eblock_num].element_type == 2){
+        for(int i=0;i<spec.elements.entity_blocks[eblock_num].num_elements_in_block; i++){
+          auto& triangles = spec.elements.entity_blocks[eblock_num];
 
-    //Create new holders for geometry data
-    geom = rtcNewGeometry(device, RTC_GEOMETRY_TYPE_TRIANGLE);
-    vb = (float*) rtcSetNewGeometryBuffer(geom,
-      RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, 3*sizeof(float), 3);
-    ib = (unsigned*) rtcSetNewGeometryBuffer(geom,
-      RTC_BUFFER_TYPE_INDEX, 0, RTC_FORMAT_UINT3, 3*sizeof(unsigned), 1);
-    ib[0] = 0; ib[1] = 1; ib[2] = 2;
+          //Create new holders for geometry data
+          geom = rtcNewGeometry(device, RTC_GEOMETRY_TYPE_TRIANGLE);
+          vb = (float*) rtcSetNewGeometryBuffer(geom,
+            RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, 3*sizeof(float), 3);
+          ib = (unsigned*) rtcSetNewGeometryBuffer(geom,
+            RTC_BUFFER_TYPE_INDEX, 0, RTC_FORMAT_UINT3, 3*sizeof(unsigned), 1);
+          ib[0] = 0; ib[1] = 1; ib[2] = 2;
+          //Node # of the triangle's vertices
+          n1 = triangles.data[4*i +1];
+          n2 = triangles.data[4*i +2];
+          n3 = triangles.data[4*i +3];
 
-    //Node # of the triangle's vertices
-    n1 = triangles.data[4*i +1];
-    n2 = triangles.data[4*i +2];
-    n3 = triangles.data[4*i +3];
-    
-    //Coordinates of the triangle
-    vb[0] = nodes.data[3*(n1-1)];vb[1] = nodes.data[3*(n1-1) +1];vb[2] = nodes.data[3*(n1-1)+2];
-    vb[3] = nodes.data[3*(n2-1)];vb[4] = nodes.data[3*(n2-1) +1];vb[5] = nodes.data[3*(n2-1)+2];
-    vb[6] = nodes.data[3*(n3-1)];vb[7] = nodes.data[3*(n3-1) +1];vb[8] = nodes.data[3*(n3-1)+2];
+          //Coordinates of the triangle
+          vb[0] = nodedata[3*(n1-1)];vb[1] = nodedata[3*(n1-1) +1];vb[2] = nodedata[3*(n1-1)+2];
+          vb[3] = nodedata[3*(n2-1)];vb[4] = nodedata[3*(n2-1) +1];vb[5] = nodedata[3*(n2-1)+2];
+          vb[6] = nodedata[3*(n3-1)];vb[7] = nodedata[3*(n3-1) +1];vb[8] = nodedata[3*(n3-1)+2];
 
-    for(int j=0;j<9;j++){ //Float -> Double
-      v[i][j] = vb[j];
+          v.push_back(emptyrowd);
+          for(int j=0;j<9;j++){ //Float -> Double
+            v[num_tri].push_back(vb[j]);
+          }
+
+          //Calculates centre of triangle for later use
+          centroid_array.push_back(emptyrowd);
+          centroid_array[num_tri].push_back( ( v[num_tri][0] + v[num_tri][3] + v[num_tri][6] ) / 3 );
+          centroid_array[num_tri].push_back( ( v[num_tri][1] + v[num_tri][4] + v[num_tri][7] ) / 3 );
+          centroid_array[num_tri].push_back( ( v[num_tri][2] + v[num_tri][5] + v[num_tri][8] ) / 3 );
+          normal_array.push_back(calcuate_normal(v[num_tri]));
+          area_array.push_back( area(v[num_tri]) );
+
+          rtcCommitGeometry(geom);
+          rtcAttachGeometry(scene, geom);
+          rtcReleaseGeometry(geom);
+
+          
+          //printf("Triangle %d has points: \n %f %f %f \n %f %f %f \n %f %f %f \n", num_tri,v[num_tri][0], v[num_tri][1],
+          //v[num_tri][2],v[num_tri][3],v[num_tri][4],v[num_tri][5],v[num_tri][6],v[num_tri][7],v[num_tri][8]);
+          //printf("It has area %f, normal: %f %f %f \n", area_array[num_tri], normal_array[num_tri][0],normal_array[num_tri][1],normal_array[num_tri][2]);
+          
+          //printf("Triangle %d has nodes %d %d %d, and normal:  %f %f %f \n", num_tri, n1,n2,n3,
+          //normal_array[num_tri][0],normal_array[num_tri][1],normal_array[num_tri][2]);
+          num_tri++;
+        }
     }
-
-    //Calculates centre of triangle for later use
-    centroid_array[i][0] = ( v[i][0] + v[i][3] + v[i][6] ) / 3 ;
-    centroid_array[i][1] = ( v[i][1] + v[i][4] + v[i][7] ) / 3 ;
-    centroid_array[i][2] = ( v[i][2] + v[i][5] + v[i][8] ) / 3 ;
-    save_normal( v[i], normal_array[i]);
-    area_array[i] = area(v[i]);
-
-    rtcCommitGeometry(geom);
-    rtcAttachGeometry(scene, geom);
-    rtcReleaseGeometry(geom);
   }
+
+
   rtcCommitScene(scene);
 
   vector<vector<VFdata>> view_factors;
   vector<VFdata> empty_row;
   RTCRayHit rayhit;
 
-  printf("A: Num_try = %d \n", num_tri);
   
   //Fethces the points and weights used for Gaussian quadrature integration
-  vector<vector<double>> gqpoints = get_gqpoints();
-  vector<double> gqweights = get_gqweights();
+  vector<vector<double>> gqpoints = get_gqpoints(pointnum);
+  vector<double> gqweights = get_gqweights(pointnum);
   int gqp_num = gqweights.size();
   
   //Creates the array the view factors will go into
@@ -109,7 +119,12 @@ int main(int argc, char *argv[])
   
   for(int a =0; a<num_tri;a++){//Each emmiting traingle
     for(int b=a; b<num_tri;b++){//Each possible recieving triangle
-      if(a !=b && dot_prod(normal_array[a], normal_array[b]) < 0){
+    vector<double> r_ctc;
+    r_ctc.push_back( centroid_array[b][0] - centroid_array[a][0]);
+    r_ctc.push_back( centroid_array[b][1] - centroid_array[a][1]);
+    r_ctc.push_back( centroid_array[b][2] - centroid_array[a][2]);
+
+      if(a !=b && dot_prod(r_ctc,normal_array[a]) >= 0 && dot_prod(r_ctc,normal_array[b]) <=0 ){
         //Checks where VF should be 0
 
         //Where the ray comes from
@@ -135,60 +150,42 @@ int main(int argc, char *argv[])
 
           //printf(" Triangle %d  sees triangle %d ", a, b);
           
-          double sum_j = 0.0;
-          for(int j=0;j<gqp_num;j++){
-            //m,n, coefficients describing points chosen via Gaussian Quadrature
-            double m = gqpoints[j][0]; double n = gqpoints[j][1];
-            
-            // Set  r1 = r1(m,n)
-            double r1[3];
-            r1[0] = (1.0-m-n)*v[a][0] + m*v[a][3] + n*v[a][6];
-            r1[1] = (1.0-m-n)*v[a][1] + m*v[a][4] + n*v[a][7];
-            r1[2] = (1.0-m-n)*v[a][2] + m*v[a][5] + n*v[a][8];
-            
-            double sum_i = 0.0;
-            for(int i=0;i<gqp_num;i++){
-              //s,t, coefficients describing points chosen via Gaussian Quadrature
-              double s= gqpoints[i][0];double t=gqpoints[i][1];
-
-              //Set r2 = (1-s-t)*r20 + s*r21 + t*r22
-              double r2[3];
-              r2[0] = (1.0-s-t)*v[b][0] + s*v[b][3] + t*v[b][6];
-              r2[1] = (1.0-s-t)*v[b][1] + s*v[b][4] + t*v[b][7];
-              r2[2] = (1.0-s-t)*v[b][2] + s*v[b][5] + t*v[b][8]; 
-              
-              //Set r_vec = r2 - r1
-              double r_vec[3];
-              r_vec[0] = r2[0] - r1[0];
-              r_vec[1] = r2[1] - r1[1];
-              r_vec[2] = r2[2] - r1[2];
-
-              double wi = gqweights[i];
-              sum_i += wi*sigma(r_vec, normal_array[a], normal_array[b]);              
-            }
-            double wj = gqweights[j];
-            sum_j += wj*sum_i;
-          }
-          double f = - area_array[b]*sum_j / (4*M_PI);
-          //printf(", View Factor = %f \n", f);
+          double f = calculate_viewfactor(v[a], v[b], gqp_num, gqpoints, gqweights,
+           normal_array[a], normal_array[b], area_array[b]);
+          //double f =  area_array[b]*sum_j;
+          //double ar = area_array[a]*area_array[b]*sum_j;
 
           view_factors[a].push_back(VFdata(b, f));
           view_factors[b].push_back(VFdata(a, ( area_array[a] * f ) / area_array[b] )) ;
+          printf("Triangle %d sees triangle %d, VF= %f \n", a,b,f);
+          //printf("Area = %f\n", ar);
+
         }
         
       }
       
     }
   } 
+
   rtcReleaseScene(scene);
   rtcReleaseDevice(device);
   
-  
-  printf("C: Num_try = %d \n", num_tri);
-  
-  printdatatofile(num_nodes, num_tri, triangles.data,  view_factors);
-  
-  printf("D: Num_try = %d \n", num_tri);
+
+  /*
+  double total = 0.0;
+  double area_sum;
+  for(int t=0;t<4;t++){
+    //printf("Triangle %d sees %d triangles,", t, view_factors[t].size());
+    for(int s=0;s<view_factors[t].size();s++){
+      total += (area_array[t])* view_factors[t][s].vfactor;
+    }
+    area_sum += area_array[t];
+  }
+  total /=area_sum;
+
+  printf(" total view factor %f \n", total);
+  */
+  printdatatofile(num_tri, centroid_array,  view_factors);
 
 
   return 0;
--- functions.h
+++ functions.h
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <iostream>
 #include <fstream>
+#include <MshIO/include/mshio/mshio.h>
+#define _USE_MATH_DEFINES
 using namespace std;
 
 struct VFdata{
@@ -11,24 +13,20 @@ struct VFdata{
   }
 };
 
-double dot_prod( float* a, float* b){
-  double p = a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
-  return p;
-}
 
 double dot_prod( double* a, double* b){
   double p = a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
   return p;
 }
 
-double dot_prod( float* a, double* b){
+double dot_prod( vector<double> a, vector<double> b){
   double p = a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
   return p;
 }
 
-double len( float* n){
-  double l = sqrt( n[0]*n[0] + n[1]*n[1] + n[2]*n[2]);
-  return l;
+double dot_prod( double* a, vector<double> b){
+  double p = a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+  return p;
 }
 
 double len( double* n){
@@ -36,7 +34,12 @@ double len( double* n){
   return l;
 }
 
-double area(double* vb){
+double len( vector<double> n){
+  double l = sqrt( n[0]*n[0] + n[1]*n[1] + n[2]*n[2]);
+  return l;
+}
+
+double area(vector<double> vb){
   double a;
   double r01[3];
   r01[0] = vb[0] - vb[3]; r01[1] = vb[1] - vb[4]; r01[2] = vb[2] - vb[5];
@@ -46,7 +49,7 @@ double area(double* vb){
   return a; 
 }
 
-int save_normal( double* v, double* destination){
+vector<double> calcuate_normal( vector<double> v){
  // n = (P1 - P0) X (P2 - P0)
   
   double n[3];
@@ -58,28 +61,26 @@ int save_normal( double* v, double* destination){
   double y = 1.0/x;
   double z;
   
+  vector<double> result;
   z = y*n[0];
-  destination[0] = z;
+  result.push_back(z);
   z = y*n[1];
-  destination[1] = z;
+  result.push_back(z);
   z = y*n[2];
-  destination[2] = z;
+  result.push_back(z);
   
-  return 0;
+  return result;
 }
 
-void get_centre( double* centroid, double* v, int n, double* destination ){
-  if( n == 0){
-    destination[0] = centroid[0];destination[1] = centroid[1];destination[2] = centroid[2];
+vector<double> get_gqweights(string pointnum){
+  string filename = "gauss4x4_w.txt";
+  if(pointnum == "64"){
+    filename = "gauss8x8_w.txt";
+  }else if(pointnum == "1"){
+    filename = "centroid_w.txt";
+  }else if(pointnum == "6"){
+    filename = "strang4_w.txt";
   }
-  else{
-    destination[0] = 0.5*( centroid[0] + v[ 3*(n-1)]);
-    destination[1] = 0.5*( centroid[1] + v[ 3*(n-1) + 1]);
-    destination[2] = 0.5*( centroid[2] + v[ 3*(n-1) + 2]);
-  }
-}
-
-vector<double> get_gqweights(string filename = "gauss4x4_w.txt"){
   vector<double> w;
   ifstream win_file;
   win_file.open(filename);
@@ -94,7 +95,17 @@ vector<double> get_gqweights(string filename = "gauss4x4_w.txt"){
   return w;
 }
 
-vector<vector<double>> get_gqpoints(string filename = "gauss4x4_x.txt"){
+vector<vector<double>> get_gqpoints(string pointnum){
+  string filename = "gauss4x4_x.txt";
+  if(pointnum == "64"){
+    filename = "gauss8x8_x.txt";printf("64 point sampling\n");
+  }else if(pointnum == "1"){
+    filename = "centroid_x.txt";printf("1 point sampling\n");
+  }else if(pointnum == "6"){
+    filename = "strang4_x.txt";printf("6 point sampling\n");
+  }else{
+    printf("16 point sampling\n");
+    }
   vector<vector<double>> points;
   double input_double;
   ifstream xin_file;
@@ -121,23 +132,85 @@ vector<vector<double>> get_gqpoints(string filename = "gauss4x4_x.txt"){
   return points;
 }
 
-void printdatatofile(int num_nodes, int num_tri, vector<long unsigned int> trianglesdata, vector<vector<VFdata>> view_factors){
+void printdatatofile(int num_tri, vector<vector<double>> centroids, vector<vector<VFdata>> view_factors){
   ofstream OutputFile("viewfactorsdata.txt");
-  OutputFile << "#Nodes" << endl;
-  OutputFile << num_nodes << endl;
   OutputFile << "#Triangles" << endl;
   OutputFile << num_tri << endl;
   OutputFile << "Triangle Data" << endl;
   for(int i=0;i<num_tri;i++){
 
     OutputFile << i << " ";
-    OutputFile << trianglesdata[4*i +1] << " " << trianglesdata[4*i+2] << " " << trianglesdata[4*i +3];
+    OutputFile << centroids[i][0] << " " << centroids[i][1] << " " << centroids[i][2];
     int num_seen = view_factors[i].size();
     //printf("Triangle %d sees %d other triangles \n", i, num_seen);
     for(int alpha = 0; alpha < num_seen;alpha++){
-      OutputFile << " f" << view_factors[i][alpha].target << " " << view_factors[i][alpha].vfactor;
+      OutputFile << " " << view_factors[i][alpha].target << " " << view_factors[i][alpha].vfactor;
     }
     OutputFile << endl;
   }
   OutputFile.close();
+}
+
+vector<double> getnodedata(mshio::MshSpec spec){
+  vector<double> nodedata;
+  for(int i=0;i<spec.nodes.num_entity_blocks;i++){
+      for(int j=0;j<spec.nodes.entity_blocks[i].data.size();j++){
+        nodedata.push_back(spec.nodes.entity_blocks[i].data[j]);
+      }
+    }
+  return nodedata;
+}
+
+double sigma(double* r, vector<double> normal_a, vector<double> normal_b){
+  // Equal to Cos(g1)*Cos(g2) / r*r
+  double f =  dot_prod(r, normal_a) * dot_prod(r, normal_b);
+  f /= pow( dot_prod(r, r), 2 );
+  return f;
+}
+
+double calculate_viewfactor(vector<double> va, vector<double> vb, int gqp_num, vector<vector<double>> gqpoints,
+ vector<double> gqweights, vector<double> normal_a, vector<double> normal_b, double area_b ){
+  double sum_j = 0.0;
+  for(int j=0;j<gqp_num;j++){
+    //m,n, coefficients describing points chosen via Gaussian Quadrature
+    double m = gqpoints[j][0]; double n = gqpoints[j][1];
+            
+    double wj = gqweights[j];
+            
+    // Set  r1 = r1(m,n)
+    double r1[3];
+    r1[0] = (1.0-m-n)*va[0] + m*va[3] + n*va[6];
+    r1[1] = (1.0-m-n)*va[1] + m*va[4] + n*va[7];
+    r1[2] = (1.0-m-n)*va[2] + m*va[5] + n*va[8];
+            
+    double sum_i = 0.0;
+    for(int i=0;i<gqp_num;i++){
+      //s,t, coefficients describing points chosen via Gaussian Quadrature
+      double s= gqpoints[i][0];double t=gqpoints[i][1];
+
+      //Set r2 = (1-s-t)*r20 + s*r21 + t*r22
+      double r2[3];
+      r2[0] = (1.0-s-t)*vb[0] + s*vb[3] + t*vb[6];
+      r2[1] = (1.0-s-t)*vb[1] + s*vb[4] + t*vb[7];
+      r2[2] = (1.0-s-t)*vb[2] + s*vb[5] + t*vb[8]; 
+              
+      //Set r_vec = r2 - r1
+      double r_vec[3];
+      r_vec[0] = r2[0] - r1[0];
+      r_vec[1] = r2[1] - r1[1];
+      r_vec[2] = r2[2] - r1[2];
+
+      double wi = gqweights[i];
+      sum_i += wi*sigma(r_vec, normal_a, normal_b);
+      //printf("For m=%f,n=%f,s=%f,t=%f,wj=%f,wi=%f,sum_i=%f\n",m,n,s,t,wj,wi,sum_i);            
+    }
+    sum_j += wj*sum_i;
+  }
+  return  - area_b*sum_j / M_PI;
+}
+
+int sumonetonminusone(int n){
+  int count = 0;
+  for(int i=1;i<n;i++){count += i;}
+  return count;
 }
